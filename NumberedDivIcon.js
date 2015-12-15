ffa660f47b9636fcc444d5ab638d76df478431f5
Oscar Fonts <oscar.fonts@gmail.com>
2015-12-15 08:01:55 +0100

    Add clustering so that nearby pins cluster together. Closes #5

---
+++ NumberedDivIcon.js
@@ -0,0 +1,18 @@
+// From: https://gist.github.com/comp615/2288108
+L.NumberedDivIcon = L.Icon.extend({
+    options: {
+        number: ''
+    },
+
+    createIcon: function () {
+        var div = document.createElement('div');
+        var img = this._createImg(this.options['iconUrl']);
+        var numdiv = document.createElement('div');
+        numdiv.setAttribute("class", "number");
+        numdiv.innerHTML = this.options['number'] || '';
+        div.appendChild(img);
+        div.appendChild(numdiv);
+        this._setIconStyles(div, 'icon');
+        return div;
+    }
+});
--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 // initialize the map
-var map = L.map('map').setView([20, 0], 2);
+var map = L.map('map', {maxZoom: 18}).setView([20, 0], 2);
 
 // load GeoJSON polygon data from an external file
 $.getJSON("RNbasemap.geojson", function(basemap) {
@@ -10,7 +10,7 @@ $.getJSON("RNbasemap.geojson", function(basemap) {
 $.getJSON("twitterdata.geojson", function (data) {
     // add GeoJSON layer to the map once the file is loaded
     var mapIcon = L.icon({
-        iconUrl: 'map-marker-icon-small.png',
+        iconUrl: 'map-marker.png',
         shadowUrl: 'shadow50.png',
         iconSize: [27, 40],
         popupAnchor: [0, -15],
@@ -18,13 +18,31 @@ $.getJSON("twitterdata.geojson", function (data) {
         shadowAnchor: [12, 42]
     });
 
+    var clustered = L.markerClusterGroup({
+        iconCreateFunction: function(cluster) {
+            return new L.NumberedDivIcon({
+                iconUrl: 'map-cluster.png',
+                shadowUrl: 'shadow50.png',
+                iconSize: [27, 40],
+                popupAnchor: [0, -15],
+                shadowSize: [50, 64],
+                shadowAnchor: [12, 42],
+                number: cluster.getChildCount()
+            });
+        }
+    });
+
     L.geoJson(data, {
         pointToLayer: function (feature, latlng) {
             var marker = L.marker(latlng, {icon: mapIcon});
-            var html = '<div class="twitter"><img class="placeholder" src="http://placehold.it/14"/>' + feature.properties.Name + '</div>';
-            html += '<div class="addr"><img class="placeholder" src="http://placehold.it/14"/>' + feature.properties.Match_addr + '</div>';
+            var html = '<div class="twitter"><img class="placeholder" src="http://placehold.it/14" width="14"/>' + feature.properties.Name + '</div>';
+            html += '<div class="addr"><img class="placeholder" src="http://placehold.it/14" width="14"/>' + feature.properties.Match_addr + '</div>';
             marker.bindPopup(html);
+            clustered.addLayer(marker);
             return marker;
         }
-    }).addTo(map);
+    });
+
+    map.addLayer(clustered);
+
 });
